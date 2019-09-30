9038d0c7881e2ed2bcc9101c49e472d5207fcd0d
罗敏文 <luominwen@Minwen.local>
2019-09-30 14:31:09 +0800

    拜访记录应用修改为模块

--- myaddons/access_record/__manifest__.py
+++ myaddons/access_record/__manifest__.py
@@ -4,12 +4,12 @@
     'author': 'caibei',
     'depends': [
         'base',
+        'crm'
     ],
-    'application': True,
+    'application': False,
     'data': [
         'security/access_record_security.xml',
         'security/ir.model.access.csv',
-        'views/access_record_menu.xml',
         'views/access_record_views.xml',
         'views/customer_views.xml',
     ]
